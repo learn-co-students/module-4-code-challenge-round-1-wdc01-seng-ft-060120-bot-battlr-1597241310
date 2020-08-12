4b4aab16ba951511dfdf771400bb03e10aa0184d
Tien Phan <tienphan1989@gmail.com>
2020-08-12 11:46:26 -0400

    ‘submission’

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -10,38 +10,44 @@ const botTypeClasses = {
 };
 
 const BotCard = props => {
+  const {bot, displayRobot} = props;
+
+  const handleClick = () => {
+    displayRobot(props.key)
+  }
+
   return (
     <div className="ui column">
       <div
         className="ui card"
-        key={props.bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        key={props.key}
+        onClick={handleClick}
       >
         <div className="image">
-          <img alt="oh no!" src={props.bot.avatar_url} />
+          <img alt="oh no!" src={bot.avatar_url} />
         </div>
         <div className="content">
           <div className="header">
             {props.bot.name}
-            <i className={botTypeClasses[props.bot.bot_class]} />
+            <i className={botTypeClasses[bot.bot_class]} />
           </div>
           <div className="meta text-wrap">
-            <small>{props.bot.catchphrase}</small>
+            <small>{bot.catchphrase}</small>
           </div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat" />
-            {props.bot.health}
+            {bot.health}
           </span>
 
           <span>
             <i className="icon lightning" />
-            {props.bot.damage}
+            {bot.damage}
           </span>
           <span>
             <i className="icon shield" />
-            {props.bot.armor}
+            {bot.armor}
           </span>
           <span>
             <div className="ui center aligned segment basic">
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,68 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
+
+const url = `http://localhost:3000/bots`;
 
 class BotsPage extends Component {
-  //start here with your code for step one
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      bots: null,
+      displayBots: [],
+      myArmyBots: [],
+      currentBot: null
+    }
+  }
+  
+  componentDidMount() {
+    this.fetchBots();
+  }
+
+  fetchBots = () => {
+    fetch(url)
+    .then(resp => resp.json())
+    .then(resp => this.setState({
+      displayBots: resp
+    }))
+  }
+
+  clickRobot = (id) => {
+    let newArmy = this.state.myArmyBots.slice();
+    newArmy.push(id);
+    this.setState({
+      bots: true,
+      myArmyBots: newArmy,
+      currentBot: null
+    })
+  }
+
+  displayRobot = (id) => {
+    this.setState({
+      bots: false,
+      currentBot: id
+    })
+  };
+
+  allBots = () => {
+    this.setState({
+      currentBot: null,
+      bots: true
+    })
+  };
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    let displayedBot = this.state.currentBot ? this.state.displayBots.find(bot => bot.id === this.state.currentBot) : null;
+
+    return( 
+    <div>
+    <BotCollection displayBots={this.state.displayBots} displayRobot={this.displayRobot} clickRobot={this.clickRobot}/>
+    <BotSpecs bot={displayedBot} clickRobot={this.clickRobot} bots={this.allBots}/>
+    <YourBotArmy myArmyBots={this.state.myArmyBots} displayRobot={this.displayRobot} displayBots={this.state.displayBots}/>
+    </div>
+    )
   }
 }
 
