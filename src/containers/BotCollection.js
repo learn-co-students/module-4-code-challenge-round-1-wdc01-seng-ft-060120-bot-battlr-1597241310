e93f735f3cc0dfd2418f6116af2f73709c162163
jbaxter6 <jbaxter6@radford.edu>
2020-08-12 12:39:18 -0400

    Need to finish Advanced deliverables

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
+import BotCard from '../components/BotCard.js'
 
 class BotCollection extends Component {
   //your code here
   generateBots = () => {
+    
     this.props.bots.map(bot =>
       <BotCard 
       bot={bot}
-      addBot={props.addBot}
+      moveBot={props.addBot}
+      deleteBot={props.deleteBot}
       />)
   }
 
+  
   render() {
     return (
       <div className="ui four column grid">
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import BotCollection from './BotsPage.js';
+import YourBotArmy from './YourBotArmy.js';
 
 const API = 'http://localhost:6001/bots/'
 
@@ -25,13 +26,57 @@ class BotsPage extends Component {
 
   addToMyBots = (bot) => {
     debugger
+    let botList = [...this.state.myBots]
+      if(!botList.includes(bot)){
+        this.setState({
+          myBots: botList
+        })
+    }
+  }
+
+  removeBotFromList = (bot) => {
+    // i wouldve used debugger lol
+    let botList = [...this.state.myBots]
+
+      if(botList.includes(bot)){
+        this.setState({
+          myBots: botList.filter(thisBot =>
+            bot !== thisBot)
+        })
+      }
+  }
+
+  deleteBotFromService = (bot) => {
+    // i wouldve used debugger lol
+    let botId = bot.id
+
+    if(botList.includes(bot)){
+      this.setState({
+        myBots: botList.filter(thisBot =>
+          bot !== thisBot)
+      })
+    }
+    // deleteFetch
+    fetch(API + botId, {
+      method: 'DELETE'
+    })
+    .then(resp => resp.json())
   }
   
 
   render() {
     return (
     <div>
-      <BotCollection bots={this.state.bots} addBots={this.addToMyBots}/>
+      <YourBotArmy 
+      bots={this.state.bots}
+      myBots={this.state.myBots}
+      removeBots={this.removeBotFromList}
+      />
+      <BotCollection 
+      bots={this.state.bots} 
+      addBots={this.addToMyBots}
+      deleteBot={this.deleteBotFromService}
+      />
     </div>
     )
   }
