709d461edf3a72de6d95b3801262d2a8c1c8370d
Issa sangare <issabmsangare@hotmail.com>
2020-08-12 11:02:08 -0400

    first commit

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import BotSpecs from "../components/BotSpecs"
+
+
 
 class BotCollection extends Component {
   //your code here
-
+  let 
   render() {
     return (
       <div className="ui four column grid">
         <div className="row">
           {/*...and here..*/}
+           <BotSpecs />
           Collection of all bots
         </div>
       </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy"
 
+let API = "http://localhost:6001/bots"
 class BotsPage extends Component {
   //start here with your code for step one
+  constructor(){
+    super()
+    this.state ={
+      bots: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(bots => {
+      this.setState({
+        bots
+      })
+    })
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+      <YourBotArmy />
+      <BotCollection AllBots={this.state.bots}/>
+      
+      
+      
+      
+      </div>;
   }
 }
 
