952809af3406aa3e2af6d2e1a23f08f9d6273412
jbaxter6 <jbaxter6@radford.edu>
2020-08-12 10:27:29 -0400

    hello

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
+import BotCollection from './BotsPage.js';
+
+const API = 'http://localhost:6001/bots/'
 
 class BotsPage extends Component {
   //start here with your code for step one
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       bots: [],
+       myBots: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(bots => 
+      this.setState({
+        bots: bots
+      }))
+  }
+  
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return (
+    <div>
+      <BotCollection />
+    </div>
+    )
   }
 }
 
